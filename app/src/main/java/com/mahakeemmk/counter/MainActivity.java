8fb902259853b1369fd171dc9e2d169b81e6d2e9
MAHakeem-MK <dev.mahakeemmk@gmail.com>
2017-11-23 16:13:04 +0530

    vibrate on thasbeeh limit

--- app/src/main/java/com/mahakeemmk/counter/MainActivity.java
+++ app/src/main/java/com/mahakeemmk/counter/MainActivity.java
@@ -1,12 +1,15 @@
 package com.mahakeemmk.counter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Vibrator;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Vibrator vibe;
     private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     public void count(View view) {
         count++;
         display(count);
+        if (count != 0 && count % 33 == 0) {
+            vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+            vibe.vibrate(300);
+        }
     }
     
     public void resetCount(View view) {
