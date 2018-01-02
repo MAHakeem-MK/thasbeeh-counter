65bc64d2cb626f10474f7e66a60a2f32905bc9d6
MAHakeem-MK <dev.mahakeemmk@gmail.com>
2018-01-02 15:52:53 +0530

    confirmation dialog for reset.

--- app/src/main/java/com/mahakeemmk/counter/MainActivity.java
+++ app/src/main/java/com/mahakeemmk/counter/MainActivity.java
@@ -1,11 +1,14 @@
 package com.mahakeemmk.counter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.os.Vibrator;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Vibrator vibe;
@@ -37,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
     }
     
     public void resetCount(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Do you really want to reset?");
+        alertDialogBuilder.setPositiveButton("Reset",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        confirmReset();
+                        Toast.makeText(MainActivity.this,"Count reset", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Cancelled",Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    protected void confirmReset() {
         int count;
         count = 0;
         display(count);
