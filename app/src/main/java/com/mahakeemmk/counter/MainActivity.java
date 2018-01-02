999afba4e93ca20eec8460f3eb4e03fa0e71072a
MAHakeem-MK <dev.mahakeemmk@gmail.com>
2018-01-02 07:09:05 +0530

    count lost on device rotate is solved.

--- app/src/main/java/com/mahakeemmk/counter/MainActivity.java
+++ app/src/main/java/com/mahakeemmk/counter/MainActivity.java
@@ -6,33 +6,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.os.Vibrator;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Vibrator vibe;
-    private int count = 0;
+    private TextView value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        value = (TextView)findViewById(R.id.value);
+        vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (savedInstanceState!=null) {
+            display(savedInstanceState.getInt("count_value"));
+        }
     }
 
     private void display(Integer number) {
-        TextView value = (TextView)findViewById(R.id.value);
         value.setText(number.toString());
     }
 
     public void count(View view) {
+        int count = Integer.parseInt(value.getText().toString());
         count++;
         display(count);
         if (count != 0 && count % 33 == 0) {
-            vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
             vibe.vibrate(300);
         }
     }
     
     public void resetCount(View view) {
+        int count;
         count = 0;
         display(count);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        int count = Integer.parseInt(value.getText().toString());
+        if (count!=0) {
+            outState.putInt("count_value",count);
+        }
     }
 }
